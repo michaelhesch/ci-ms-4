73a0a8b0cdced6f537018249bff88a459e1a2eb0
michaelhesch <michael.hesch@gmail.com>
2021-12-03 18:06:02 +0000

    Refactor landing page and add store page template & view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ class HomeView(ListView):
     model = Product
     paginate_by = 6
     template_name='index.html'
+
+
+class StoreView(ListView):
+    model = Product
+    paginate_by = 6
+    template_name='store.html'
