082676e355b4f11989c95ff8e0248c3bd0bee1f1
michaelhesch <michael.hesch@gmail.com>
2021-12-12 17:54:17 +0000

    Update migrations and rebuild database locally, update pagination settings and minor styling revisions to storefront

--- home/views.py
+++ home/views.py
@@ -4,11 +4,11 @@ from product.models import Product
 
 class HomeView(ListView):
     model = Product
-    paginate_by = 6
+    paginate_by = 8
     template_name='index.html'
 
 
 class StoreView(ListView):
     model = Product
-    paginate_by = 6
+    paginate_by = 8
     template_name='store.html'
