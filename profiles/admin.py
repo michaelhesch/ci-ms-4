dfc65b7729097618198b61958797e4051f1d3ba8
michaelhesch <michael.hesch@gmail.com>
2021-12-13 00:36:23 +0000

    Set up vendor profile model and admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, VendorProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
+
     list_display = (
         'user',
         'verified',
         'created_on',
+    )
+
+
+class VendorProfileAdmin(admin.TabularInline):
+    model = VendorProfile
+    
+    list_display = (
+        'store_owner',
+        'store_name',
+        'vendor_balance',
     )
 
 
