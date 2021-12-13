8b6bf6ad491512ae4139a3fd7fc43f4befae52b7
michaelhesch <michael.hesch@gmail.com>
2021-12-13 18:32:23 +0000

    Refactor userprofiles models, add vendor store view and template

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.dispatch import receiver
+from django.utils.text import slugify
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 from django_countries.fields import CountryField
 
 
-class UserProfile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone = models.CharField(max_length=16, null=True, blank=True)
@@ -16,6 +17,16 @@ class UserProfile(models.Model):
     default_state = models.CharField(max_length=50, null=True, blank=True)
     default_zipcode = models.CharField(max_length=25, null=True, blank=True)
     default_country = CountryField(blank_label='Select Country', null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.user.username
+
+
+class UserProfile(Profile):
     avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     avatar_url = models.URLField(null=True, blank=True)
     verified = models.BooleanField(default=False)
@@ -41,13 +52,22 @@ def create_update_profile(sender,instance, created, **kwargs):
     instance.userprofile.save()
 
 
-class VendorProfile(models.Model):
-    store_owner = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    store_name = models.CharField(max_length=100, null=True, blank=True)
-    vendor_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+class VendorProfile(UserProfile):
+    store_name = models.CharField(max_length=100, null=True, blank=True, unique=True)
+    store_slug = models.SlugField(max_length=200)
+    vendor_balance_unpaid = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    vendor_balance_paid = models.DecimalField(max_digits=20, decimal_places=2, default=0)
 
     class Meta:
         verbose_name_plural = 'Vendor Profiles'
 
     def __str__(self):
         return self.store_name
+
+    def save(self, *args, **kwargs):
+        slug_name = str(self.store_name)
+        self.store_slug = slugify(slug_name)
+        super(VendorProfile, self).save(*args, **kwargs)
+
+    def get_vendor_slug(self):
+        return self.store_slug
--- profiles/views.py
+++ profiles/views.py
@@ -5,11 +5,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import UserProfile, VendorProfile
 from checkout.models import Order
+from product.models import Product
 
 
 class ProfileView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
         profile = UserProfile.objects.get(user=self.request.user)
+        vendor_profile = VendorProfile.objects.get(user=self.request.user)
         orders = Order.objects.filter(user=self.request.user, ordered=True)
 
         paginator = Paginator(orders, 6)
@@ -19,16 +21,27 @@ class ProfileView(LoginRequiredMixin, View):
             'avatar': profile.avatar_url,
             'orders': orders,
             'profile': profile,
+            'vendor': vendor_profile,
             'page_obj': paginator,
         }
 
         return render(self.request, "profile.html", context)
 
 
-class VendorProfileView(LoginRequiredMixin, ListView):
+class VendorProfileView(LoginRequiredMixin, View):
     model = VendorProfile
-    paginate_by = 6
-    template_name='store.html'
+    template_name='vendor_store.html'
+
+    def get(self, *args, **kwargs):
+        store_slug = self.kwargs.get('store_slug')
+        users_store = VendorProfile.objects.get(store_slug=store_slug)
+        products = Product.objects.filter(seller=users_store.user_id)
+
+        context = {
+            'products': products,
+            'store': users_store,
+        }
+        return render(self.request, "vendor_store.html", context)
 
 
 class OrderHistoryView(LoginRequiredMixin, ListView):
