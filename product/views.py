c74fb04bb7c329b3add432740e181c207cdb5bac
michaelhesch <michael.hesch@gmail.com>
2021-12-14 21:25:26 +0000

    Add ability to edit product details for existing products - update views, urls and templates

--- product/views.py
+++ product/views.py
@@ -65,8 +65,67 @@ class ListProduct(LoginRequiredMixin, View):
         context = {
             'form': form,
         }
-
         return render(self.request, 'add_product.html', context)
+
+
+class EditProduct(LoginRequiredMixin, View):
+
+    def get(self, *args, **kwargs):
+        seller = VendorProfile.objects.get(user=self.request.user)
+        slug = self.kwargs['slug']
+        product = Product.objects.get(slug=slug, seller=seller)
+
+        form_init = model_to_dict(product)
+        form = ProductForm(initial=form_init)
+        context = {
+            'slug': slug,
+            'form': form,
+        }
+
+        return render(self.request, 'edit_product.html', context)
+    
+    def post(self, *args, **kwargs):
+        slug = self.kwargs['slug']
+        seller_userprofile = VendorProfile.objects.get(user=self.request.user)
+        store_slug = seller_userprofile.vendorprofile.store_slug
+        existing_data = Product.objects.get(slug=slug, seller=seller_userprofile)
+
+        if self.request.method == 'POST':
+            try:
+                form = ProductForm(self.request.POST, self.request.FILES)
+                if form.is_valid():
+                    category = form.cleaned_data.get('category')
+                    product_name = form.cleaned_data.get('product_name')
+                    description = form.cleaned_data.get('description')
+                    brand = form.cleaned_data.get('brand')
+                    boost_clock = form.cleaned_data.get('boost_clock')
+                    memory_clock = form.cleaned_data.get('memory_clock')
+                    memory_type = form.cleaned_data.get('memory_type')
+                    interface_type = form.cleaned_data.get('interface_type')
+                    price = form.cleaned_data.get('price')
+
+                    existing_data.category = category
+                    existing_data.product_name = product_name
+                    existing_data.description = description
+                    existing_data.brand = brand
+                    existing_data.boost_clock = boost_clock
+                    existing_data.memory_clock = memory_clock
+                    existing_data.memory_type = memory_type
+                    existing_data.interface_type = interface_type
+                    existing_data.price = price
+                    existing_data.image_url = existing_data.image_url
+
+                    existing_data.save()
+
+                    messages.success(self.request, "Your product is updated!")
+                    return redirect(reverse("profiles:vendorprofile", kwargs={'store_slug': store_slug}))
+                else:
+                    messages.warning(self.request, "Your product was not updated,\
+                        please try again")
+                    return redirect(reverse("profiles:vendorprofile", kwargs={'store_slug': store_slug}))
+            except Exception as e:
+                messages.error(self.request, f"An unexpected error occured: {e}")
+                return redirect(reverse("profiles:vendorprofile", kwargs={'store_slug': store_slug}))
 
 
 class ReviewProduct(LoginRequiredMixin, View):
@@ -89,33 +148,24 @@ class ReviewProduct(LoginRequiredMixin, View):
         return render(self.request, 'review_product.html', context)
     
     def post(self, *args, **kwargs):
-        print(self.request.POST)
         form = ProductReview(self.request.POST or None)
         product_slug = self.kwargs['slug']
         product = Product.objects.get(slug=product_slug)
-        
-        form_data = {
-            'title': self.request.POST['title'],
-            'body_content': self.request.POST['body_content'],
-            'rating': self.request.POST['rating']
-        }
+
         try:
             if form.is_valid():
                 title = self.request.POST['title']
                 body_content = self.request.POST['body_content']
                 added_by = self.request.user
                 rating = self.request.POST['rating']
-                print(rating)
                 product_reviewed = product
 
                 review = Review.objects.get_or_create(product_reviewed=product, added_by=added_by)[0]
-                print(review)
                 review.title = title
                 review.body_content = body_content
                 review.added_by = added_by
                 review.rating = rating
                 review.product_reviewed = product_reviewed
-                print(review)
                 review.save()
 
                 return redirect(reverse("product:product_detail", kwargs={'slug': product_slug}))
@@ -145,3 +195,19 @@ def remove_item_from_store(request, sku):
         except Exception as e:
             messages.error(request, f"Error (500): {e}")
             return HttpResponse(status=500)
+
+
+@login_required
+def delete_review(request, sku):
+    product = Product.objects.get(sku=sku)
+    product_slug = product.slug
+    review = Review.objects.get(product_reviewed=product, added_by=request.user)
+
+    try:
+        if request.method == 'POST':
+            review.delete()
+            messages.success(request, "Your review has been deleted.")
+            return redirect(reverse("product:product_detial", kwargs={'slug': product_slug}))
+    except Exception as e:
+        messages.error(request, f"An unexpected error occured: {e}")
+        return redirect(reverse("product:product_detail", kwargs={'slug': product_slug}))
